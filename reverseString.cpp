3b34aff29b20683ea20ac45f46f548cd499e8386
roy2pushkar <roy2000pushkar@gmail.com>
2023-03-22 13:44:06 +0530

    prepinsta100codes

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
 using namespace std;
-string reverseWord(string str , int n){
+string reverseWord(string str ){
 
   //Your code here
   //int n=str.length();
-  int st=0;
-  int end=n-1;
-  while(st<=end){
-      swap(str[st++],str[end--]);
+  //int st=0;
+  //int end=n-1;
+  int n=str.length();
+  for(int i=0;i<n/2;i++{
+      swap(str[i],str[n-i-1]);
   }
   return str;
 
 }
 
 int main()
-{   int n;
-    cin>>n;
+{   //int n;
+   // cin>>n;
     string str;
-    cin>>str;
-    cout<<reverseWord(str , n)<<endl;
+    getline(cin,str);
+    //cin>>str;
+    cout<<reverseWord(str )<<str<<endl;
 
     return 0;
 }
